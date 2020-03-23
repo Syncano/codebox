69579ed80f9bff84e2adc9fba97c0c9e2d8410fd
Robert Kopaczewski <rk@23doors.com>
2020-03-23 18:44:44 +0100

    smarter worker-setup

--- deploy/worker-setup.sh
+++ deploy/worker-setup.sh
@@ -1,27 +1,41 @@
-#! /bin/bash
+#!/bin/bash
 set -euo pipefail
 
-EXEC=(nsenter -t 1 -m -u -i -n -p --)
-
-STARTUP_SCRIPT=' 
-FUSE_INSTALLED=$(dpkg -s fuse | grep Status)
-XFS_INSTALLED=$(dpkg -s xfsprogs | grep Status)
-
-if [ -z "$FUSE_INSTALLED" ] || [ -z "$XFS_INSTALLED" ]; then
-    echo "Installing fuse and xfsprogs"
-    apt-get update
-    apt-get install fuse xfsprogs
+if [ "$#" -eq 0 ]; then
+    EXEC=(nsenter -t 1 -m -u -i -n -p --)
+    "${EXEC[@]}" bash "$0" "nsenter"
+    exit
 fi
 
-XFS_MOUNTED=$(mount | grep /mnt/codebox)
+# If dpkg is present, install fuse and xfs
+if hash dpkg 2>/dev/null; then
+    FUSE_INSTALLED=$(dpkg -s fuse | grep Status)
+    XFS_INSTALLED=$(dpkg -s xfsprogs | grep Status)
 
-if [ -z "$XFS_MOUNTED" ]; then
-    echo "Preparing and mounting /mnt/codebox"
+    if [ -z "$FUSE_INSTALLED" ] || [ -z "$XFS_INSTALLED" ]; then
+        echo "Installing fuse and xfsprogs"
+        apt-get update
+        apt-get install -y --no-install-recommends fuse xfsprogs
+    fi
+else
+    >&2 echo "! dpkg unavailable, skipping fuse and xfsprogs install"
+fi
+
+# Check if /mnt/codebox already mounted, if not mount it and format
+if ! mount | grep -q /mnt/codebox; then
     mkdir -p /mnt/codebox
-    umount $DOCKER_DEVICE
-    mkfs.xfs -f $DOCKER_DEVICE
-    mount -o pquota $DOCKER_DEVICE /mnt/codebox
-fi
-'
 
-"${EXEC[@]}" bash -c "${STARTUP_SCRIPT}"
+    if [ -z "$DOCKER_DEVICE" ] || [ -e "$DOCKER_DEVICE" ]; then
+        >&2 echo "! docker device not set or does not exist, skipping mounting"
+        exit 0
+    fi
+
+    if ! hash mkfs.xfs 2>/dev/null; then
+        >&2 echo "! mkfs.xfs unavailable, skipping mounting"
+        exit 0
+    fi
+
+    umount "$DOCKER_DEVICE"
+    mkfs.xfs -f "$DOCKER_DEVICE"
+    mount -o pquota "$DOCKER_DEVICE" /mnt/codebox
+fi
