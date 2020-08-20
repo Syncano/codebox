bca14847a340edacd015d82b68e6a23880dc2061
Robert Kopaczewski <rk@23doors.com>
2020-08-20 20:15:59 +0200

    chore: changed grpc port to 9000 (#134)

--- cmd/broker.go
+++ cmd/broker.go
@@ -38,7 +38,7 @@ As there is no authentication, always run it in a private network.`,
 	Flags: []cli.Flag{
 		&cli.IntFlag{
 			Name: "port", Aliases: []string{"p"}, Usage: "port for grpc server",
-			EnvVars: []string{"PORT"}, Value: 8000,
+			EnvVars: []string{"PORT"}, Value: 9000,
 		},
 		&cli.IntFlag{
 			Name: "uwsgi-port", Usage: "port for uwsgi server",
@@ -58,7 +58,7 @@ As there is no authentication, always run it in a private network.`,
 		// Broker Server options.
 		&cli.StringSliceFlag{
 			Name: "lb-addrs", Usage: "load balancer TCP addresses",
-			EnvVars: []string{"LB_ADDRS"}, Value: cli.NewStringSlice("127.0.0.1:8000"),
+			EnvVars: []string{"LB_ADDRS"}, Value: cli.NewStringSlice("127.0.0.1:9000"),
 		},
 		&cli.IntFlag{
 			Name: "lb-retry", Usage: "number of retries on failed lb run",
